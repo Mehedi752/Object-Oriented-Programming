40747910b18ae3ff82b06b8bd939e0383cf34496
Mehedi <mehedihasansagor301@example.com>
2024-11-16 16:17:54 +0600

    complete day - 05

--- Java Day - 05/OverRiding2.java
+++ Java Day - 05/OverRiding2.java
@@ -18,7 +18,7 @@ class child extends parent{
 
 public class OverRiding2 {
     public static void main(String[] args) {
-        // parent ob = new parent(30, 40);
+        parent ob = new parent(30, 40);
         child myOb = new child(10, 20);
     }
 }
